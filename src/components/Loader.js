bbb57eac892f88d2f5002fa390f5b7ccba9226a0
ian-chong <46514872+ian-chong@users.noreply.github.com>
2019-01-29 18:23:12 +0800

    i have installed bootstrap correctly

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Loader = () => <img src={"./react-nextagram/src/components/fruits-lemon.gif"} alt='This page is loading right now, can you wait please?' height='42' width='42'/>
 
--- src/containers/UserImages.js
+++ src/containers/UserImages.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Card, CardImg, CardText, CardBody,
